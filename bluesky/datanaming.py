c399c8e14c3e473d74fcea83cfda6e690e8e8576
Pavol Juhas <pavol.juhas@gmail.com>
2015-11-13 18:26:49 -0500

    Docstrings for the DataNaming class.
    Add DataNaming.default_template class attribute.

--- bluesky/datanaming.py
+++ bluesky/datanaming.py
@@ -1,39 +1,78 @@
 #!/usr/bin/env python
 
 
+"""Generation of file names for each event in databroker header.
+"""
+
+
+import os.path
+import re
+
+
 class DataNaming(object):
+    """
+    Build filenames from fields in databroker header.
 
-    """Build filenames from fields in databroker header.
+    Attributes
+    ----------
+    default_template : str, class attribute
+        Default value for the `template` when not specified.
 
-    prefix   -- fixed path that is prepended to the generated name.
-    template -- string template where curly brackets will
-                be exanded according to Python format minilanguage.
-                Template segments denoted with "<>" such as "<segment>"
-                are silently omitted, when their template expansion fails.
+    template : str
+        filename template where curly brackets are filled with entries from
+        databroker header using the Python format mini-language.  Template
+        segments denoted with "<>", e.g., '<segment>' are silently omitted,
+        when their expansion fails.  The template expressions recognize the
+        following variables:
 
-    Template expressions are expanded with the following variable names:
+        h    -- Header
+        e    -- Event object from the Header h.
+        N    -- sequence number of the Event e.
+        start    -- abbreviation for h.start
+        stop     -- abbreviation for h.stop if it exists
+        scan_id  -- abbreviation for h.start.scan_id
 
-    h        -- Header object from the databroker.
-    e        -- Event object from the Header h.
-    N        -- sequence number of the Event e.
-    start    -- abbreviation for h.start
-    stop     -- abbreviation for h.stop if it exists
-    scan_id  -- abbreviation for h.start.scan_id
+    prefix : str
+        constant output directory that is prepended to the generated name.
+
+    Parameters
+    ----------
+    template : str, optional
+        Set the `template` attribute.  Use the `default_template` value
+        by default.
+    prefix : str, optional
+        Set the fixed ouput path `prefix`.
     """
 
-    prefix = ''
-    _template = 'scan{scan_id:05d}_{N:03d}<-T{e.data[cs700]:03.1f}>.tiff'
+    default_template = 'scan{scan_id:05d}_{N:03d}<-T{e.data[cs700]:03.1f}>.tiff'
 
-    def __init__(self, template=None, prefix=None):
-        if template is not None:
-            self.template = template
-        if prefix is not None:
-            self.prefix = prefix
+
+    def __init__(self, template=None, prefix=''):
+        self.template = self.default_template if template is None else template
+        self.prefix = prefix
         return
 
 
+    def __repr__(self):
+        nm = type(self).__name__
+        fmt = "{0}(template={1.template!r}, prefix={1.prefix!r})"
+        return fmt.format(nm, self)
+
+
     def __call__(self, h):
-        """Generate names from fields in databroker header h."""
+        """
+        Generate names from fields in databroker header h.
+
+        Parameters
+        ----------
+        h : Header
+            a header from the databroker
+
+        Returns
+        -------
+        list
+            Filenames generated for the header.
+        """
         from dataportal import get_events
         tparts = self._split_template(self.template)
         td = dict(h=h, start=h.start, scan_id=h.start.scan_id)
@@ -44,14 +83,18 @@ class DataNaming(object):
         return rv
 
 
-    def __repr__(self):
-        s = "DataNaming(template={!r}, prefix={!r})"
-        return s.format(self.template, self.prefix)
-
+    _template = None
 
     @property
     def template(self):
-        """Template for generating names from databroker fields.
+        """
+        Template for generating names from databroker fields.
+
+        Raises
+        ------
+        ValueError
+            When a new template does not include event index {N}
+            or if it has unbalanced segment markers '<>'.
         """
         return self._template
 
@@ -63,7 +106,6 @@ class DataNaming(object):
 
 
     def _makename(self, tparts, td):
-        import os.path
         nmparts = []
         for seg, isopt in tparts:
             try:
@@ -80,7 +122,6 @@ class DataNaming(object):
     @staticmethod
     def _validate_template(t):
         "Raise ValueError for invalid template string."
-        import re
         hasN = re.search(r'\{N\b', t)
         if not hasN:
             raise ValueError("template must include '{N}'")
@@ -98,10 +139,12 @@ class DataNaming(object):
     def _split_template(t):
         """Split template string at '<segment>' markers.
 
-        Return a list of (segment, isoptional) pairs where isoptional
-        flag marks up optional template components.
+        Returns
+        -------
+        list
+            Template split into a list of `(component, optional)` pairs
+            where the `optional` flag marks up an optional components.
         """
-        import re
         segments = [[]]
         barebrac = re.split(r'(\{[^}]*\})', t)
         isopt = False
@@ -126,6 +169,5 @@ class DataNaming(object):
             s = ''.join(seg)
             if s:  rv.append((s, isopt))
         return rv
-
 
 # class DataNaming

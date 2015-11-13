e9dfde924d6ce3dce595ebd3bb35635c90ff0484
Pavol Juhas <pavol.juhas@gmail.com>
2015-11-12 19:19:48 -0500

    Fix typos.  Add support for event selection.

--- bluesky/tiffexporter.py
+++ bluesky/tiffexporter.py
@@ -22,22 +22,24 @@ class TiffExporter(object):
 
     def __str__(self):
         "Human-readable configuration of this object."
-        lines = ["TiffExporter() configuration:",
+        lines = ["TiffExporter() attributes",
                  "  fetch = {self.fetch}",
                  "  naming = {self.naming}",
                  "  set_mtime = {self.set_mtime}",
                  "  mtime_window = {self.mtime_window}",]
-        rv = "\n".join(lines).format(self)
+        rv = "\n".join(lines).format(self=self)
         return rv
 
 
-    def __call__(self, h, dryrun=False, overwrite=False):
+    def __call__(self, h, select=None, dryrun=False, overwrite=False):
         """Export sequentially-numbered TIFF files from databroker header.
 
         Parameters
         ----------
         h : Header
             a header from the databroker
+        select : array of integer indices or a slice object like numpy.s_[-3:]
+            Save outputs only from the events at selected indices.
         dryrun : bool
             When True, display what would be done without taking
             any action.
@@ -51,46 +53,64 @@ class TiffExporter(object):
             dryordo = lambda msg, f, *args, **kwargs:  print(msg)
         else:
             dryordo = lambda msg, f, *args, **kwargs:  f(*args, **kwargs)
+        setmtime = lambda f, t: os.utime(f, (os.path.getatime(f), t))
         noop = lambda : None
         msgrm = "remove existing output {}"
-        msgskip = "skip {f} as it is already saved as {o}"
+        msgskip = "skip {f} already saved as {o}"
         outputfiles = self.naming(h)
-        imgs = self.fetch(h)
-        events = get_events(h, fill=False)
+        eventtimes = [e.time for e in get_events(h, fill=False)]
+        n = len(eventtimes)
+        selection = _makesetofindices(n, select)
         dircache = {}
-        for f, img, e in zip(outputfiles, imgs, events):
-            existingoutputs = self.outputFileExists(f, dircache=dircache)
+        foundouts = {
+                f : self.outputFileExists(f,
+                    mtime=self.set_mtime and etime, dircache=dircache)
+                for f, etime in zip(outputfiles, eventtimes)}
+        imgs = self.fetch(h)
+        for i, f, img, etime in zip(range(n), outputfiles, imgs, eventtimes):
+            if not i in selection:  continue
+            existingoutputs = foundouts[f]
             # skip this image when overwrite is False
             if not overwrite:
                 for o in existingoutputs:
                     print(msgskip.format(f=f, o=o))
-                continue
-            assert overwrite
+                if existingoutputs:  continue
+            assert overwrite or not existingoutputs
             for o in existingoutputs:
                 dryordo(msgrm.format(o), os.remove, o)
-            msg = "write image as {}".format(f)
+            msg = "write image data to {}".format(f)
             dryordo(msg, tifffile.imsave, f, img)
             if self.set_mtime:
-                stinfo = os.stat(f)
-                isotime = datetime.datetime.fromtimestamp(e.time).isoformat(' ')
-                msg = "adjust mtime of {} to {}".format(f, isotime)
-                dryordo(msg, os.utime, f, (stinfo.st_atime, event.time))
+                isotime = datetime.datetime.fromtimestamp(etime).isoformat(' ')
+                msg = "adjust image file mtime to {}".format(isotime)
+                dryordo(msg, setmtime, f, etime)
         return
 
 
-    def findExistingTiffs(self, h):
+    def findExistingTiffs(self, h, select=None):
         """Return a list of already existing TIFF outputs for databroker
         header.
+
+        Parameters
+        ----------
+        h : Header
+            a header from the databroker
+        select : array of integer indices or a slice object like numpy.s_[-3:]
+            Find outputs only from the events at specified indices.
         """
         from databroker import get_events
         # avoid repeated calls to os.listdir
         dircache = {}
         filenames = self.naming(h)
-        events = get_events(h, fill=False)
+        eventtimes = [e.time for e in get_events(h, fill=False)]
+        if not self.set_mtime:
+            eventtimes = len(eventtimes) * [None]
+        n = len(eventtimes)
+        selection = _makesetofindices(n, select)
         rv = []
-        for f, e in zip(filenames, events):
-            mtime = e.time if self.set_mtime else None
-            rv.extend(self.outputFileExists(f, mtime, dircache=dircache))
+        for i, f, etime in zip(range(n), filenames, eventtimes):
+            if not i in selection:  continue
+            rv.extend(self.outputFileExists(f, etime, dircache=dircache))
         return rv
 
 
@@ -99,16 +119,16 @@ class TiffExporter(object):
 
         filename -- output filename to be checked
         mtime    -- file modification time in seconds or None.
-                    When specified, match tiff files in the
-                    filename directory with the same mtime.
+                    When specified and nonzero, match tiff files in the
+                    filename directory with mtime closer than mtime_window.
 
         Return a list of full paths of existing tiff files.
         """
         fa = os.path.abspath(filename)
         rv = [fa] if os.path.exists(filename) else []
-        if mtime is None:  return rv
+        if not mtime:  return rv
         outputdir = os.path.abspath(os.path.dirname(filename))
-        ext = os.path.splitext
+        ext = os.path.splitext(filename)
         dircache = dircache if dircache is not None else {}
         if not outputdir in dircache:
             ofiles = [os.path.join(outputdir, f)
@@ -121,7 +141,7 @@ class TiffExporter(object):
                 assert rv[0] == f
                 continue
             dt = abs(os.path.getmtime(f) - mtime)
-            if dt < self._mtime_window:
+            if dt < self.mtime_window:
                 rv.append(f)
         return rv
 
@@ -141,7 +161,7 @@ class TiffExporter(object):
         if isinstance(value, str):
             from functools import partial
             from databroker import get_images
-            self._fetch = partial(get_images, value)
+            self._fetch = partial(get_images, name=value)
         elif callable(value):
             self._fetch = value
         else:
@@ -150,3 +170,11 @@ class TiffExporter(object):
         return
 
 # class TiffExporter
+
+
+def _makesetofindices(n, select):
+    import numpy
+    indices = numpy.arange(n)
+    if select is not None:
+        indices = indices[select]
+    return set(indices)
